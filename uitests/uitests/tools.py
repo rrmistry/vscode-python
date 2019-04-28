668886ef4717abcbffa857b1d9399cb02f2d8f83
Don Jayamanne <don.jayamanne@yahoo.com>
2019-04-28 15:45:54 -0700

    No logging

--- uitests/uitests/tools.py
+++ uitests/uitests/tools.py
@@ -35,9 +35,10 @@ def retry(exceptions, tries=100, delay=0.1, backoff=1):
             while mtries > 1:
                 try:
                     return f(*args, **kwargs)
-                except exceptions as e:
-                    msg = "{}, Retrying in {} seconds...".format(e, mdelay)
-                    logging.info(msg)
+                except exceptions:
+                    # except exceptions as e:
+                    #     msg = "{}, Retrying in {} seconds...".format(e, mdelay)
+                    #     logging.info(msg)
                     time.sleep(mdelay)
                     mtries -= 1
                     mdelay *= backoff
